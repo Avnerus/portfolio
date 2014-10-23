0e1246757d92cad45e7b5b13810717b457cb1f5d
Avner Peled <avnerus@gmail.com>
2014-10-23 13:48:14 -0400

    Koala done!

--- js/works/koala.js
+++ js/works/koala.js
@@ -21,6 +21,7 @@ Koala.prototype.init = function (opts, stage, clickHandler) {
     this.eventEmitter = require('../event_manager').getEmitter();
 
     this.loadAnimations();
+    this.routine();
 }
 
 
@@ -36,7 +37,7 @@ Koala.prototype.loadAnimations = function(name, frames) {
     this.walkLeft = new PIXI.MovieClip(frameSequence);
     this.walkLeft.anchor.x = 0.5;
     this.walkLeft.anchor.y = 0.5;
-    this.walkLeft.position.x =  1100;
+    this.walkLeft.position.x =  1200;
     this.walkLeft.position.y = 570;
     //dance.scale = {x: 0.25, y: 0.25};
     this.walkLeft.loop = true;
@@ -44,19 +45,110 @@ Koala.prototype.loadAnimations = function(name, frames) {
 
     this.walkLeft.buttonMode = true;
     this.walkLeft.setInteractive(true);
+    
+    // Turn from left to write
+    frameSequence = [];
+    for (var i = 5; i >= 0; i--) {
+        var num = MathUtil.pad(i,2);
+        var texture = new PIXI.Texture.fromFrame("KoalaO2L" + num + ".png");
+        frameSequence.push(texture);
+    }
+    for (var i = 0; i <= 5; i++) {
+        var num = MathUtil.pad(i,2);
+        var texture = new PIXI.Texture.fromFrame("KoalaO2R" + num + ".png");
+        frameSequence.push(texture);
+    }
+    this.turnRight = new PIXI.MovieClip(frameSequence);
+    this.turnRight.anchor.x = 0.5;
+    this.turnRight.anchor.y = 0.5;
+    this.turnRight.position.x =  900;
+    this.turnRight.position.y = 570;
+    this.turnRight.animationSpeed = 0.25;
+    this.turnRight.buttonMode = true;
+    this.turnRight.setInteractive(true);
+    this.turnRight.loop = false;
+    
+    frameSequence = [];
+    // Walk right
+    for (var i = 0; i < 12; i++) {
+        var num = MathUtil.pad(i,2);
+        var texture = new PIXI.Texture.fromFrame("KoalaRight" + num + ".png");
+        frameSequence.push(texture);
+    }
+    this.walkRight = new PIXI.MovieClip(frameSequence);
+    this.walkRight.anchor.x = 0.5;
+    this.walkRight.anchor.y = 0.5;
+    this.walkRight.position.x =  900;
+    this.walkRight.position.y = 570;
+    this.walkRight.loop = true;
+    this.walkRight.animationSpeed = 0.25;
 
-    this.walkLeft.play();
+    this.walkRight.buttonMode = true;
+    this.walkRight.setInteractive(true);
+
+    // Turn from right to left
+    frameSequence = [];
+    for (var i = 5; i >= 0; i--) {
+        var num = MathUtil.pad(i,2);
+        var texture = new PIXI.Texture.fromFrame("KoalaO2R" + num + ".png");
+        frameSequence.push(texture);
+    }
+    for (var i = 0; i <= 5; i++) {
+        var num = MathUtil.pad(i,2);
+        var texture = new PIXI.Texture.fromFrame("KoalaO2L" + num + ".png");
+        frameSequence.push(texture);
+    }
+    this.turnLeft = new PIXI.MovieClip(frameSequence);
+    this.turnLeft.anchor.x = 0.5;
+    this.turnLeft.anchor.y = 0.5;
+    this.turnLeft.position.x =  1200;
+    this.turnLeft.position.y = 570;
+    this.turnLeft.animationSpeed = 0.25;
+    this.turnLeft.buttonMode = true;
+    this.turnLeft.setInteractive(true);
+    this.turnLeft.loop = false;
 
 
-    var self = this;
-
-    /*dance.click  = function(mouseData){
-      console.log("GAMAD CLICK");
+    this.walkLeft.click = this.turnRight.click = this.walkRight.click = this.turnLeft.click = function(mouseData){
+      console.log("KOALA CLICK");
       self.eventEmitter.emit('work_clicked', self);
     }
 
-    dance.play();*/
-    this.stage.addChild(this.walkLeft);
+    var self = this;
+
+}
+
+
+Koala.prototype.routine = function() {
+    var self = this;
+    var t1 = new TimelineMax({repeat: -1});
+    // Walk left
+    t1.call(function() {
+        self.stage.addChild(self.walkLeft);
+        self.walkLeft.play();
+    });
+    t1.to(this.walkLeft.position, 5, {ease: Linear.easeNone, x:900});
+    t1.call(function() {
+        self.stage.removeChild(self.walkLeft);
+        self.stage.addChild(self.turnRight);
+        self.turnRight.gotoAndPlay(0);
+    });
+    t1.to(this.walkLeft.position, 1.5, {ease: Linear.easeNone}); // Do nothing for 1.5 seconds
+    t1.call(function() {
+        self.stage.removeChild(self.turnRight);        
+        self.stage.addChild(self.walkRight);
+        self.walkRight.play();
+    });
+    t1.to(this.walkRight.position, 5, {ease: Linear.easeNone, x:1200});
+    t1.call(function() {
+        self.stage.removeChild(self.walkRight);        
+        self.stage.addChild(self.turnLeft);
+        self.turnLeft.gotoAndPlay(0);
+    });
+    t1.to(this.walkRight.position, 1.5, {ease: Linear.easeNone}); // Do nothing for 1.5 seconds
+    t1.call(function() {
+       self.stage.removeChild(self.turnLeft); 
+    });
 }
 Koala.prototype.update = function() {
     
